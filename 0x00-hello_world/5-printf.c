2c34ea05fc54a8282b48ce432a8bdaef15c63ae2
Wonder717 <wonderdermwin@yahoo.com>
2022-10-13 16:56:33 -0700

    C Basics

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#includ <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	prints("%s", "with proper grammar, but this outcome is a piece of art,\n");
+	return (o);
+}
