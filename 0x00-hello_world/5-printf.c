e6b854e123f62f93aef8484b893dcf29fd471396
Wonder Agudah <85680026+Wonder717@users.noreply.github.com>
2022-10-14 00:09:06 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  * main - Entry point
  *
@@ -9,6 +7,6 @@
  */
 int main(void)
 {
-	printf("%s", "with proper grammar, but this outcome is a piece of art,\n");
-	return (o);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
