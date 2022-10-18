1145cde8ef6d4f12eed4973669171eb5b4980857
Wonder Agudah <85680026+Wonder717@users.noreply.github.com>
2022-10-18 16:13:05 +0000

    Examples of nested loops in C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,23 @@
-#include "main.h"
-
+#include "holberton.h"
 
 /**
- * main - Lanzador
- *
- * Return: Always 0
- *
- */
+* main - Lanzador
+*
+* Return: Always 0
+*
+*/
 
-
- int main(void)
- {
-
-         _putchar('H');
-	 _putchar('o');
-	 _putchar('l');
-	 _putchar('b');
-	 _putchar('e');
-	 _putchar('r');
-	 _putchar('t');
-	 _putchar('o');
-	 _putchar('r');
-	 _putchar('n');
-	 _putchar('\n');
-	 return (o);
- }
- 
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
