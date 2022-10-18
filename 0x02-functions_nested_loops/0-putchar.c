06b6acf168404538c3c25f230ffdfc5217917a21
Wonder717 <wonderdermwin@yahoo.com>
2022-10-18 08:44:41 -0700

    A program that returns zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include "holbrton.h"
+
+
+/**
+ * main - Lanzador
+ *
+ * Return: Always 0
+ *
+ */
+
+
+ int main(void)
+ {
+
+         _putchar('H');
+	 _putchar('o');
+	 _putchar('l');
+	 _putchar('b');
+	 _putchar('e');
+	 _putchar('r');
+	 _putchar('t');
+	 _putchar('o');
+	 _putchar('r');
+	 _putchar('n');
+	 _putchar('\n');
+	 return (o);
+
+		 
+ 
+ 
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Examples of nested loops in C
